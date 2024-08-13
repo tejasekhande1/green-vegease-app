28ae0d7735cf7853b86dc73f19c721fe93c33b12
Tejas Ekhande <tejasekhande01@gmail.com>
2024-08-13 23:31:52 +0530

    GV-4 | Remove redundant white color variable

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -5,4 +5,3 @@ const kColorBlack = Color.fromRGBO(24, 23, 37, 1);
 const kColorGrey = Color.fromRGBO(124, 124, 124, 1);
 const kColorWhite = Color.fromRGBO(255, 255, 255, 1);
 const kColorLiteWhite = Color.fromRGBO(252, 252, 252, 0.7);
-const kColorGetWhite = Color.fromRGBO(255, 249, 255, 1);
