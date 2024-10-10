a25fccc5300c539adf64aced9dffe3ea8cc83840
pratikphad007 <pratikphad9997@gmail.com>
2024-10-10 21:46:59 +0530

    GV-35 | Home Screen Scaffold solving

--- lib/features/app-ui/homescreen/presentation/pages/home_screen_page.dart
+++ lib/features/app-ui/homescreen/presentation/pages/home_screen_page.dart
@@ -118,94 +118,96 @@ class _HomeScreenPageState extends State<HomeScreenPage> {
     };
 
     Map<dynamic, String> location = {'Location': 'Pune India'};
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          margin: EdgeInsets.only(top: 58.29.h),
-          child: Image.asset(
-            Constants.kHomeIc,
-            width: 26.48.w,
-            height: 30.8.h,
-            fit: BoxFit.fill,
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.location_on,
-              color: const Color.fromRGBO(76, 79, 77, 1),
-              size: 25.sp,
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 58.29.h),
+            child: Image.asset(
+              Constants.kHomeIc,
+              width: 26.48.w,
+              height: 30.8.h,
+              fit: BoxFit.fill,
             ),
-            Text(
-              //get value from API
-              location['Location']!,
-              style: kTextStyleGilroy600.copyWith(
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.location_on,
                 color: const Color.fromRGBO(76, 79, 77, 1),
-                fontSize: 18.sp,
+                size: 25.sp,
               ),
+              Text(
+                //get value from API
+                location['Location']!,
+                style: kTextStyleGilroy600.copyWith(
+                  color: const Color.fromRGBO(76, 79, 77, 1),
+                  fontSize: 18.sp,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: 364.w,
+            height: 51.57.h,
+            margin: EdgeInsets.only(top: 20.h),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.sp),
+              color: const Color.fromRGBO(242, 243, 242, 0.7),
             ),
-          ],
-        ),
-        Container(
-          width: 364.w,
-          height: 51.57.h,
-          margin: EdgeInsets.only(top: 20.h),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.sp),
-            color: const Color.fromRGBO(242, 243, 242, 0.7),
+            child: TextField(
+              controller: searchcontroller,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: const Color.fromRGBO(124, 124, 124, 1),
+                    size: 36.sp,
+                  ),
+                  hintText: 'Search Store',
+                  hintStyle: kTextStyleGilroy600.copyWith(
+                    color: const Color.fromRGBO(124, 124, 124, 1),
+                    fontSize: 14.sp,
+                  ),
+                  contentPadding: EdgeInsets.all(15.h)),
+            ),
           ),
-          child: TextField(
-            controller: searchcontroller,
-            decoration: InputDecoration(
-                border: InputBorder.none,
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: const Color.fromRGBO(124, 124, 124, 1),
-                  size: 36.sp,
-                ),
-                hintText: 'Search Store',
-                hintStyle: kTextStyleGilroy600.copyWith(
-                  color: const Color.fromRGBO(124, 124, 124, 1),
-                  fontSize: 14.sp,
-                ),
-                contentPadding: EdgeInsets.all(15.h)),
-          ),
-        ),
-        SizedBox(height: 20.h),
-        SizedBox(
-          height: 600.h,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 114.99.h,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
-                    itemCount: offercradList.length,
-                    itemBuilder: (context, index) => OfferCard(
-                      cardouterimage: offercradList[index]['CardOuterImage']!,
-                      cardBgImage: offercradList[index]['CardBgImage']!,
-                      cardimage: offercradList[index]['CardImage']!,
-                      cardtitle: offercradList[index]['Cardtitle']!,
-                      cardoffer: offercradList[index]['Cardoffer']!,
+          SizedBox(height: 20.h),
+          SizedBox(
+            height: 600.h,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 114.99.h,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: offercradList.length,
+                      itemBuilder: (context, index) => OfferCard(
+                        cardouterimage: offercradList[index]['CardOuterImage']!,
+                        cardBgImage: offercradList[index]['CardBgImage']!,
+                        cardimage: offercradList[index]['CardImage']!,
+                        cardtitle: offercradList[index]['Cardtitle']!,
+                        cardoffer: offercradList[index]['Cardoffer']!,
+                      ),
                     ),
                   ),
-                ),
-                BannerCard(
-                    products: products['Exclusive Offer']!,
-                    productcardname: 'Exclusive Offer'),
-                BannerCard(
-                    products: products['Best Selling']!,
-                    productcardname: 'Best Selling'),
-                Glosseries(products: products['Glosseries']!)
-              ],
+                  BannerCard(
+                      products: products['Exclusive Offer']!,
+                      productcardname: 'Exclusive Offer'),
+                  BannerCard(
+                      products: products['Best Selling']!,
+                      productcardname: 'Best Selling'),
+                  Glosseries(products: products['Glosseries']!)
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
